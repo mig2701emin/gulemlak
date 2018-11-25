f179d3e497545cedc5625e7225032b39be4d1c99
ibrahimgul2701@gmail.com <ibrahimgul2701@gmail.com>
2018-11-25 12:00:11 +0300

    favicon

--- application/views/layout/styles.php
+++ application/views/layout/styles.php
@@ -11,7 +11,26 @@
 <link rel="stylesheet" type="text/css" href="<?php echo base_url('assets/');?>css/style.css">
 
 
-<link rel="shortcut icon" href="<?php echo base_url('assets/');?>images/favicon.png">
-<link rel="apple-touch-icon" href="<?php echo base_url('assets/');?>images/apple-touch-icon.png">
-<link rel="apple-touch-icon" sizes="72x72" href="<?php echo base_url('assets/');?>images/apple-touch-icon-72x72.png">
-<link rel="apple-touch-icon" sizes="114x114" href="<?php echo base_url('assets/');?>images/apple-touch-icon-114x114.png">
+<!--<link rel="shortcut icon" href="<?php /*echo base_url('assets/');*/?>images/ico/favicon.png">
+<link rel="apple-touch-icon" href="<?php /*echo base_url('assets/');*/?>images/ico/apple-touch-icon.png">
+<link rel="apple-touch-icon" sizes="72x72" href="<?php /*echo base_url('assets/');*/?>images/ico/apple-touch-icon-72x72.png">
+<link rel="apple-touch-icon" sizes="114x114" href="<?php /*echo base_url('assets/');*/?>images/ico/apple-touch-icon-114x114.png">
+-->
+
+<link rel="apple-touch-icon" sizes="57x57" href="<?php echo base_url('assets/');?>images/ico/apple-icon-57x57.png">
+<link rel="apple-touch-icon" sizes="60x60" href="<?php echo base_url('assets/');?>images/ico/apple-icon-60x60.png">
+<link rel="apple-touch-icon" sizes="72x72" href="<?php echo base_url('assets/');?>images/ico/apple-icon-72x72.png">
+<link rel="apple-touch-icon" sizes="76x76" href="<?php echo base_url('assets/');?>images/ico/apple-icon-76x76.png">
+<link rel="apple-touch-icon" sizes="114x114" href="<?php echo base_url('assets/');?>images/ico/apple-icon-114x114.png">
+<link rel="apple-touch-icon" sizes="120x120" href="<?php echo base_url('assets/');?>images/ico/apple-icon-120x120.png">
+<link rel="apple-touch-icon" sizes="144x144" href="<?php echo base_url('assets/');?>images/ico/apple-icon-144x144.png">
+<link rel="apple-touch-icon" sizes="152x152" href="<?php echo base_url('assets/');?>images/ico/apple-icon-152x152.png">
+<link rel="apple-touch-icon" sizes="180x180" href="<?php echo base_url('assets/');?>images/ico/apple-icon-180x180.png">
+<link rel="icon" type="image/png" sizes="192x192"  href="<?php echo base_url('assets/');?>images/ico/android-icon-192x192.png">
+<link rel="icon" type="image/png" sizes="32x32" href="<?php echo base_url('assets/');?>images/ico/favicon-32x32.png">
+<link rel="icon" type="image/png" sizes="96x96" href="<?php echo base_url('assets/');?>images/ico/favicon-96x96.png">
+<link rel="icon" type="image/png" sizes="16x16" href="<?php echo base_url('assets/');?>images/ico/favicon-16x16.png">
+<link rel="manifest" href="/manifest.json">
+<meta name="msapplication-TileColor" content="#ffffff">
+<meta name="msapplication-TileImage" content="<?php echo base_url('assets/');?>images/ico/ms-icon-144x144.png">
+<meta name="theme-color" content="#ffffff">
